57325b2197eda273c4a2930fc8fa5739421f6dd7
Sarah Lean <13692824+weeyin83@users.noreply.github.com>
2023-12-18 12:23:52 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
 
   backend "azurerm" {
-    key                  = "github-terraform.tfstate"
+    key                  = "github.terraform.tfstate"
   }
 
   required_version = ">=0.12"
