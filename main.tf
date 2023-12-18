aa4d537cdd61d091711176a125e05cad88a3a6ec
Sarah Lean <13692824+weeyin83@users.noreply.github.com>
2023-12-18 13:36:51 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,6 +36,6 @@ resource "azurerm_virtual_network" "techielassvnet" {
 resource "azurerm_subnet" "techielasssubnet" {
   name                 = "subnet"
   resource_group_name  = azurerm_resource_group.techielassrg.name
-  virtual_network_name = azurerm_virtual_network.techielassvnet
+  virtual_network_name = azurerm_virtual_network.techielassvnet.name
   address_prefixes     = ["10.0.0.0/24"]
 }
